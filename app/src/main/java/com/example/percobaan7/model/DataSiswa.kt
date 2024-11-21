51c725f8a7d4a83bf17bd94368e705b9e7a7ef47
Rauda08 <raudajnnh08@gmail.com>
2024-11-21 18:26:09 +0700

    membuat data siswa

---
+++ app/src/main/java/com/example/percobaan7/model/DataSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.percobaan7.model
+
+
+data class DataSiswa(
+    val nama: String = "",
+    val NIM: String = "",
+    val email: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val notelepon: String = ""
+)
+
+
