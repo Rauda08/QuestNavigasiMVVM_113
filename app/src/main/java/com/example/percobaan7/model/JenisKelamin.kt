2955e61bff5ecff5b6cf312fc60f4a82e42a945f
Rauda08 <raudajnnh08@gmail.com>
2024-11-21 18:24:27 +0700

    membuat data jenis kelamin

---
+++ app/src/main/java/com/example/percobaan7/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.percobaan7.model
+
+
+import com.example.percobaan7.R
+object JenisKelamin {
+    val jenisKelamin = listOf(
+            R.string.laki,
+        R.string.wanita
+    )
+}
